b1a1e4515fbcf43646ae245c71bbcf39502ac496
Ahmed <ahmed.akod@outlook.com>
2026-07-19 22:59:49 -0400

    Revert "feat: add langfuse tracing to project"
    This reverts commit 10004fcacd57895ae285e89597d350f49aebab07.

--- src/ai.ts
+++ src/ai.ts
@@ -1,11 +1,7 @@
-import "../instrumentation"
 import { ChatOpenAI } from "@langchain/openai";
 import { prompt } from "./prompt";
 import { topic } from "./topics";
-import { CallbackHandler } from "@langfuse/langchain";
-import { langfuseSpanProcessor } from "../instrumentation";
-
-const langfuseHandler = new CallbackHandler();
+import { loadEnvFile } from "process";
 
 const randomTopic = topic.topics[Math.round(Math.random() * (topic.topics.length - 1))]
 const randomStyle = topic.formats[Math.round(Math.random() * (topic.formats.length - 1))]
@@ -14,12 +10,23 @@ const agent = new ChatOpenAI({
     model: "gpt-5.4-mini",
 });
 
+try {
+    loadEnvFile();
+} catch (error) {
+    if (error instanceof Error) {
+        console.log(error.message);
+    }
+    else {
+        console.log(error)
+    }
+}
+
 export async function ridgeway_boss() {
-    // Initialize the Langfuse CallbackHandler
     const output = await agent.invoke(
-        [{ role: "system", content: prompt }, { role: "human", content: `Your topic is ${randomTopic}. Please generate the text in the following format: ${randomStyle}` }], { callbacks: [langfuseHandler] });
+        [{ role: "system", content: prompt }, { role: "human", content: `Your topic is ${randomTopic}. Please generate the text in the following format: ${randomStyle}` }]);
     console.log(output);
     // const output = { content: "Myth: chain spots always win. Fact: Ridgeway family-run gems hit different, from chai to kabab. I’ll take the local vibe, fresh flavors, and halal comfort any day." }
-    await langfuseSpanProcessor.forceFlush()
     return output;
 }
+
+ridgeway_boss()
--- test/ai.test.ts
+++ test/ai.test.ts
@@ -4,7 +4,6 @@ import { expect, test } from 'vitest'
 test('testing length of Ai generated response', async () => {
     const response = await ridgeway_boss()
     const content = response.toFormattedString()
-    console.log(content)
     // prompt specifies max 30 words, so 40 is used as an upper limit
     expect(content.split(' ').length).toBeLessThanOrEqual(40)
 })
